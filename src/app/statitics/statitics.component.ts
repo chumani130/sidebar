e73e9a8d53d55c35509f3fe96ffdfa6185f5e3cf
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2023-11-06 17:09:42 +0200

    Create statitics.component.ts

---
+++ src/app/statitics/statitics.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-statitics',
+  templateUrl: './statitics.component.html',
+  styleUrls: ['./statitics.component.scss']
+})
+export class StatiticsComponent {
+
+}
