34fa86b480989e46c003b451e7b82f9d5aa37c4b
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2023-11-07 11:32:28 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { SettingsComponent } from './settings/settings.component';
+import { MediaComponent } from './media/media.component';
+import { PagesComponent } from './pages/pages.component';
+import { CoupensComponent } from './coupens/coupens.component';
+import { StatiticsComponent } from './statitics/statitics.component';
+import { ProductsComponent } from './products/products.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
-  {path: 'dashboard', component: DashboardComponent}
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'products', component: ProductsComponent},
+  {path: 'statitics', component: StatiticsComponent},
+  {path: 'coupens', component: CoupensComponent},
+  {path: 'pages', component: PagesComponent},
+  {path: 'media', component: MediaComponent},
+  {path: 'settings', component: SettingsComponent}
 ];
 
 @NgModule({
