ef03d7e2701e7a79a4d0d8c3b47b131b3ea9fc3c
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2023-11-07 11:50:48 +0200

    Update sidenav.component.ts

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -1,4 +1,4 @@
-import { Component,  EventEmitter,  Output } from '@angular/core';
+import { Component,  EventEmitter,  HostListener,  OnInit,  Output } from '@angular/core';
 import { navbarData } from './nav-data';
 
 interface SideNavToggle {
@@ -10,12 +10,24 @@ interface SideNavToggle {
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.scss']
 })
-export class SidenavComponent {
+export class SidenavComponent implements OnInit {
 
   @Output() onToggleSideNav: EventEmitter<SideNavToggle> = new EventEmitter();
   collapsed = false;
   screenWidth = 0;
   navData = navbarData;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.screenWidth = window.innerWidth;
+    if(this.screenWidth <= 768 ) {
+      this.collapsed = false;
+      this.onToggleSideNav.emit({collapsed: this.collapsed, screenWidth: this.screenWidth});
+    }
+  }
+  ngOnInit(): void {
+      this.screenWidth = window.innerWidth;
+  }
 
   toggleCollapse(): void {
     this.collapsed = !this.collapsed;
